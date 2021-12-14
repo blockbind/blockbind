719153a5341c4353fb4f614be6a515846d31d3b4
Cerus <m.dorn.2003@web.de>
2021-12-14 02:17:17 +0100

    Cleanup + docs (Threading)

--- api/src/main/java/dev/cerus/blockbind/api/threading/Threading.java
+++ api/src/main/java/dev/cerus/blockbind/api/threading/Threading.java
@@ -7,9 +7,19 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 
+/**
+ * Multithreading utility
+ */
 public class Threading {
 
+    /**
+     * Thread id counter
+     */
     private static int threadId = 0;
+
+    /**
+     * Thread cache
+     */
     private static final ExecutorService exec = Executors.newCachedThreadPool(r -> {
         Thread thread = new Thread(r, String.format("BlockBind-Worker-%d", threadId++));
         thread.setUncaughtExceptionHandler((t, e) ->
@@ -20,14 +30,30 @@ public class Threading {
     private Threading() {
     }
 
+    /**
+     * Cease all operations
+     */
     public static void shutdown() {
         exec.shutdown();
     }
 
+    /**
+     * Offload a task from the main thread
+     *
+     * @param runnable The task to run async
+     */
     public static void runAsync(final Runnable runnable) {
         exec.execute(runnable);
     }
 
+    /**
+     * Convenience method for using the "correct" Threading#whenComplete() method based on the current environment
+     *
+     * @param future The future to wait for
+     * @param <T>    Future type
+     *
+     * @return A new future
+     */
     public static <T> CompletableFuture<T> whenComplete(final CompletionStage<T> future) {
         if (Identity.getIdentity().isMainThread()) {
             return whenCompleteSync(future);
@@ -36,6 +62,14 @@ public class Threading {
         }
     }
 
+    /**
+     * Returns a new future which will complete synchronously when the provided future completes
+     *
+     * @param future The future that we will wait for
+     * @param <T>    Future type
+     *
+     * @return A new future
+     */
     public static <T> CompletableFuture<T> whenCompleteSync(final CompletionStage<T> future) {
         final CompletableFuture<T> fut = new CompletableFuture<>();
         future.whenComplete((t, throwable) ->
@@ -49,6 +83,14 @@ public class Threading {
         return fut;
     }
 
+    /**
+     * Returns a new future which will complete asynchronously when the provided future completes
+     *
+     * @param future The future that we will wait for
+     * @param <T>    Future type
+     *
+     * @return A new future
+     */
     public static <T> CompletableFuture<T> whenCompleteAsync(final CompletionStage<T> future) {
         final CompletableFuture<T> fut = new CompletableFuture<>();
         future.whenComplete((t, throwable) ->
